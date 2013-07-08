2b74c059cd8e4f436cbec180021a1e5ba2825e3e
William Yager <will.yager@gmail.com>
2013-07-07 22:54:55 -0500

    Updated description of randomizer
    Added some operational description

--- Randomizer.cpp
+++ Randomizer.cpp
@@ -34,7 +34,15 @@ union data_union Randomizer::get(){
         result.dwords[i] = this->data.dwords[i];
     }
     
-    //Makes it so that something leaking data won't give up the contents of the entropy pool (or its hash, which is used the next time some entropy is added).
+    //Makes it so that something leaking data won't give up the contents of the entropy pool 
+    //(or its hash, which is used the next time some entropy is added).
+    //For example (assuming we didn't do the following step): 
+    //1.Program on PC gets 256 bits of entropy from device. Now it knows the state of the randomizer
+    //  (if it happens to request from the right part of the random data stream)
+    //2.Now, if the program on the PC (maybe an attacker) can make predictions about the next few hundred/thousand
+    //  bits of environmental data, it can determine the next 256 bits of pseudorandom data! Bad stuff!
+    //Instead, this next step should theoretically obfuscate the state of the randomizer (provided the
+    //  attacker was not able to predict environmental data from the beginning).
     result.bytes[0] += 1;
     sha_buffer_contents(result);
     
